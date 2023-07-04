27aee4306bfcabaf5072d6c4a82e477b3f29d3dd
NicolaDeSimone <nick_ale89@hotmail.it>
2023-07-04 13:22:25 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from sqlalchemy import ForeignKey
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import date
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app,
